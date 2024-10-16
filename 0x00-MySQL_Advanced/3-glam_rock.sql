b754f429194ce46365a255c633063dc034754ada
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-10-16 15:50:59 -0700

    Best band ever!

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+SELECT band_name, 
+       CASE 
+           WHEN split IS NOT NULL THEN split - formed
+           ELSE 2022 - formed
+       END AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
