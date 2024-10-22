2678859ee5892eb9f2f970fdad2fa9cdb55f1465
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-10-22 11:37:45 -0700

    Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+a Python function that returns the list of school
+having a specific topic.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Schools by topic
+    """
+    topic_filter = {
+            'topics': {
+                '$elemMatch': {
+                    '$eq': topic,
+                },
+            },
+        }
+    return [doc for doc in mongo_collection.find(topic_filter)]
